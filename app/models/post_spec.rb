3c5f3ee05aafe0234f853edc946e6f3f91d15a9f
github username <github email address>
2018-11-19 22:29:06 -0500

    Done.

---
+++ app/models/post_spec.rb
@@ -0,0 +1,8 @@
+require 'rails-helper'
+
+describe Post
+  it 'can be created' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post).to be_valid
+  end
+end
